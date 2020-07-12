e100eaf8f23f5099a419d7432a1caa1d096c345e
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-12 17:56:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function add(a, b) {
   return a + b;
 }
+
+function subsctract(a, b) {
+  return b - a;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n++;
+}
+
+function decrement(n) {
+  return n--;
+}
+
+function makeInt(n) {
+  return parseInt(n);
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
